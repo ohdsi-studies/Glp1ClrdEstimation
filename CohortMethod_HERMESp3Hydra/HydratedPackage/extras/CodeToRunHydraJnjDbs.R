6052adc31f78753146d82311528b6680e8c9b0e8
Mitch Conover <mconove1@its.jnj.com>
2021-08-20 03:18:49 -0400

    Pushing Package #4 (hydrated) - drug-specific GLP comparisons against DPP

--- CohortMethod_HERMESp3Hydra/HydratedPackage/extras/CodeToRunHydraJnjDbs.R
+++ CohortMethod_HERMESp3Hydra/HydratedPackage/extras/CodeToRunHydraJnjDbs.R
@@ -30,7 +30,7 @@ connectionDetails <- DatabaseConnector::createConnectionDetails(
         user = keyring::key_get("redShiftUserName"),
         password = keyring::key_get("redShiftPassword"),
         port = 5439,
-        server = "ohda-prod-1.cldcoxyrkflo.us-east-1.redshift.amazonaws.com/truven_mdcd", # change DB extension to refer to the DB you want to analyze
+        server = "ohda-prod-1.cldcoxyrkflo.us-east-1.redshift.amazonaws.com/jmdc", # change DB extension to refer to the DB you want to analyze
         extraSettings = "ssl=true&sslfactory=com.amazon.redshift.ssl.NonValidatingFactory"
 )
 
@@ -38,15 +38,20 @@ connectionDetails <- DatabaseConnector::createConnectionDetails(
 #cdmDatabaseSchema <- "cdm_truven_ccae_v1676"
 #cdmDatabaseSchema <- "cdm_optum_extended_dod_v1679"
 #cdmDatabaseSchema <- "cdm_truven_mdcd_v1561"
-cdmDatabaseSchema <- "cdm_optum_ehr_v1562"
-
+#cdmDatabaseSchema <- "cdm_optum_ehr_v1562"
+#cdmDatabaseSchema <- "cdm_iqvia_pharmetrics_plus_v1670"
+#cdmDatabaseSchema <- "cdm_truven_mdcr_v1692"
+cdmDatabaseSchema <- "cdm_jmdc_v1678"
 
 # STEP 5e: The name of the database schema and table where the study-specific cohorts will be instantiated:
 cohortDatabaseSchema <- "scratch_mconove1" # change to your scratch space
 #cohortTable <- "HERMES_p3_TruvenCCAE" #Revise cohortTable name to refer to the specific DB being run
 #cohortTable <- "HERMES_p3_OptumDOD" #Revise cohortTable name to refer to the specific DB being run
 #cohortTable <- "HERMES_p3_TruvenMDCD" #Revise cohortTable name to refer to the specific DB being run
-cohortTable <- "HERMES_p3_OptumEHR" #Revise cohortTable name to refer to the specific DB being run
+#cohortTable <- "HERMES_p3_OptumEHR" #Revise cohortTable name to refer to the specific DB being run
+#cohortTable <- "HERMES_p3_PharmetricsPlus" #Revise cohortTable name to refer to the specific DB being run
+#cohortTable <- "HERMES_p3_TruvenMDCR" #Revise cohortTable name to refer to the specific DB being run
+cohortTable <- "HERMES_p3_JMDC" #Revise cohortTable name to refer to the specific DB being run
 
 
 # STEP 5f: Some DB-specific meta-information that will be used by the export function:
@@ -59,9 +64,18 @@ cohortTable <- "HERMES_p3_OptumEHR" #Revise cohortTable name to refer to the spe
 # databaseId <- "TruvenMDCD"
 # databaseName <- "IBM MarketScan Multi-State Medicaid (MDCD) v1561"
 # databaseDescription <- "IBM MarketScan Multi-State Medicaid (MDCD) v1561"
-databaseId <- "OptumEHR"
-databaseName <- "Optum Pan-Therapeutic Electronic Health Records (OptumEHR) v1562"
-databaseDescription <- "Optum Pan-Therapeutic Electronic Health Records (OptumEHR) v1562"
+# databaseId <- "OptumEHR"
+# databaseName <- "Optum Pan-Therapeutic Electronic Health Records (OptumEHR) v1562"
+# databaseDescription <- "Optum Pan-Therapeutic Electronic Health Records (OptumEHR) v1562"
+# databaseId <- "PharmetricsPlus"
+# databaseName <- "IQVIA Adjudicated Health Plan Claims (PharmetricsPlus) v1670"
+# databaseDescription <- "IQVIA Adjudicated Health Plan Claims (PharmetricsPlus) v1670"
+# databaseId <- "TruvenMDCR"
+# databaseName <- "IBM MarketScan Medicare Supplemental (v1692)"
+# databaseDescription <- "IBM MarketScan Medicare Supplemental (v1692)"
+databaseId <- "JMDC"
+databaseName <- "Japan Medical Data Center (JMDC)"
+databaseDescription <- "Japan Medical Data Center (JMDC)"
 
 # For some database platforms (e.g. Oracle): define a schema that can be used to emulate temp tables:
 options(sqlRenderTempEmulationSchema = NULL)
